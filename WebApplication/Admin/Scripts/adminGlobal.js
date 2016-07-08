2554f0df37f827584352ffe7b1c60ac532d477c6
mac <macdonald.robinson@zgm.ca>
2016-07-08 10:34:23 -0600

    Added youtube plugin for tinymce editor

--- WebApplication/Admin/Scripts/adminGlobal.js
+++ WebApplication/Admin/Scripts/adminGlobal.js
@@ -488,10 +488,10 @@ $(document).ready(function () {
         plugins: [
           'advlist autolink lists link image charmap print preview hr anchor pagebreak',
           'searchreplace wordcount visualblocks visualchars code fullscreen',
-          'insertdatetime media nonbreaking save table contextmenu directionality',
+          'insertdatetime media youtube nonbreaking save table contextmenu directionality',
           'emoticons template paste textcolor colorpicker textpattern imagetools ace'
         ],
-        toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media ace',
+        toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media youtube ace',
         templates: [
         ],
         image_advtab: true,

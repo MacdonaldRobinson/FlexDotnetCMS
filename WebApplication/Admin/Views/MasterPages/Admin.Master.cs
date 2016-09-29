6dc74ec669ceeb85952854b95ade3b592d65319d
mac <macdonald.robinson@zgm.ca>
2016-09-29 09:17:35 -0600

    Removed old comments

--- WebApplication/Admin/Views/MasterPages/Admin.Master.cs
+++ WebApplication/Admin/Views/MasterPages/Admin.Master.cs
@@ -12,19 +12,9 @@ namespace WebApplication.Admin
         private Website currentWebsite = WebsitesMapper.GetWebsite();
         private long numberOfActiveLanguages = LanguagesMapper.GetAllActive().Count();
 
-        /*protected override void OnInit(EventArgs e)
-        {
-            base.OnInit(e);
-        }*/
-
         protected void Page_Init(object sender, EventArgs e)
         {
             InitPage();
-        }
-
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            //BindSiteTreeView();
         }
 
         private void InitPage()
@@ -42,172 +32,15 @@ namespace WebApplication.Admin
                 GlossaryTermsNavItem.Visible = true;
             }
 
-            /*var allNodes = SiteTree.GetAllNodes();
-            var nodes = allNodes.Where(i => (i.Value == currentWebsite.ID.ToString() || i.Value == currentWebsite.Media.ParentMediaID.ToString()));
-
-            foreach (var node in nodes)
-            {
-                node.ExpandParents();
-                node.Expand();
-            }
-
-            if (AdminBasePage.SelectedMediaDetail != null)
-            {
-                var selectedMediaDetailId = AdminBasePage.SelectedMediaDetail.ID.ToString();
-                var node = allNodes.Find(i => i.Value == selectedMediaDetailId);
-                if (node != null)
-                {
-                    node.ExpandParents();
-                    node.Select();
-                }
-            }*/
-
             if (numberOfActiveLanguages < 2)
                 LanguageSwitcher.Visible = false;
             else
                 LanguageSwitcher.Visible = true;
         }
 
-        /*private void BindTree(IMediaDetail mediaDetail, CustomTreeNode parentNode, bool renderChildren = true)
-        {
-            CustomTreeNode rootNode = new CustomTreeNode(mediaDetail.SectionTitle, mediaDetail.ID.ToString(), mediaDetail.MediaID);
-            UpdateTreeNode(rootNode, mediaDetail);
-
-            if (parentNode == null)
-            {
-                SiteTree.Nodes.Add(rootNode);
-            }
-            else
-            {
-                parentNode.ChildNodes.Add(rootNode);
-            }
-
-            if (renderChildren)
-            {
-                if (mediaDetail != null && mediaDetail.ID != 0)
-                {
-                    foreach (var child in mediaDetail.ChildMediaDetails)
-                    {
-                        BindTree(child, rootNode);
-                    }
-                }
-            }
-        }*/
-
-        private IEnumerable<Media> GetAllMedias()
-        {
-            return MediasMapper.GetDataModel().AllMedia.Where(i => i.MediaDetails.Any(j => j.MediaType.ShowInSiteTree && j.HistoryVersionNumber == 0));
-        }
-
-
-        private bool SearchWithinMediaDetail(IMediaDetail mediaDetail, string filterText)
-        {
-            if (mediaDetail == null)
-                return false;
-
-            if (mediaDetail.HistoryVersionNumber == 0 && mediaDetail.MediaID.ToString() == filterText || mediaDetail.LinkTitle.ToLower().Trim().Contains(filterText) || mediaDetail.SectionTitle.ToLower().Trim().Contains(filterText) || mediaDetail.MainContent.ToLower().Trim().Contains(filterText) || mediaDetail.MainLayout.ToLower().Trim().Contains(filterText) || mediaDetail.Fields.Any(j => j.FieldValue.Contains(filterText)))
-                return true;
-
-            foreach (var fieldAssociation in mediaDetail.Fields.SelectMany(i=>i.FieldAssociations))
-            {
-                if (fieldAssociation.MediaDetail.ID == mediaDetail.ID)
-                    continue;
-
-                if(SearchWithinMediaDetail(fieldAssociation.MediaDetail, filterText))
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        /*public void BindSiteTreeView()
-        {
-            if (Filter.Text == "")
-            {
-                var root = BaseMapper.GetDataModel().MediaDetails.FirstOrDefault(i => i.Media.ParentMedia == null && i.HistoryVersionNumber == 0);
-                BindTree(root, null);
-
-                if (SiteTree.Nodes.Count > 0)
-                {
-                    var rootNode = (CustomTreeNode)SiteTree.Nodes?[0];
-                    rootNode?.LIClasses.Add("jstree-open");
-
-                    if(rootNode.ChildNodes.Count > 0)
-                    {
-                        ((CustomTreeNode)rootNode.ChildNodes?[0])?.LIClasses.Add("jstree-open");
-                    }
-                }
-            }
-            else
-            {
-                var filterText = Filter.Text.ToLower().Trim();
-                var foundItems = GetAllMedias().Select(i => i.LiveMediaDetail).Where(i => SearchWithinMediaDetail(i, filterText));
-
-                SiteTree.Nodes.Clear();
-
-                foreach (var foundItem in foundItems)
-                {
-                    BindTree(foundItem, null, false);
-                }
-            }
-        }*/
-
         protected void LoginStatus_OnLoggedOut(object sender, EventArgs e)
         {
             this.BasePage.CheckInAll();
         }
-
-        /*private void UpdateTreeNode(CustomTreeNode node, IMediaDetail detail)
-        {
-            if (detail == null)
-                return;
-
-            node.Value = detail.ID.ToString();
-            //node.Attributes.Add("FrontEndUrl", detail.AbsoluteUrl);
-
-            var nodeText = "";
-
-            if (detail.LanguageID != AdminBasePage.CurrentLanguage.ID)
-            {
-                nodeText = detail.LinkTitle + " - " + LanguagesMapper.GetByID(detail.Language.ID).Name;
-                node.LIClasses.Add("doesNotExistInLanguage");
-            }
-            else
-            {
-                nodeText = $"{detail.LinkTitle} <small>({detail.MediaID})</small>";
-            }
-
-            if (detail.IsDeleted)
-            {
-                node.LIClasses.Add("isDeleted");
-            }
-
-            if ((!detail.ShowInMenu) && (!detail.RenderInFooter))
-                node.LIClasses.Add("isHidden");
-
-            if ((!detail.CanRender) || (!detail.IsPublished))
-                node.LIClasses.Add("unPublished");
-
-            if ((AdminBasePage.SelectedMediaDetail != null) && (AdminBasePage.SelectedMediaDetail.ID.ToString() == node.Value))
-            {
-                node.LIClasses.Add("selected");
-                node.LinkClasses.Add("jstree-clicked");
-
-                while (node.Parent != null)
-                {
-                    ((CustomTreeNode)node.Parent).LinkClasses.Add("jstree-open");
-                }
-            }
-
-            node.Text = nodeText;
-
-            //node.LinkAttributes.Add("data-frontendurl", detail.Media.PermaLink);
-            node.LinkAttributes.Add("data-frontendurl", detail.AbsoluteUrl);
-            node.LIAttributes.Add("data-mediaDetailId", detail.ID.ToString());
-
-            node.NavigateUrl = URIHelper.ConvertToAbsUrl(WebApplication.BasePage.GetRedirectToMediaDetailUrl(detail.MediaTypeID, detail.MediaID));
-        }*/
     }
 }

7aea6947553c5f65b199dd7523d5e20b84db73f4
mac <macdonald.robinson@zgm.ca>
2018-03-05 09:24:42 -0700

    Fixed issue where file manager was not loading if the url passed in was an absolute url to an external site

--- TinyFileManager.NET-master/TinyFileManager.NET/dialog.aspx.cs
+++ TinyFileManager.NET-master/TinyFileManager.NET/dialog.aspx.cs
@@ -66,7 +66,7 @@ namespace TinyFileManager.NET
                 baseUrl = protocal + Request.Url.Authority;
             }
 
-            if (File.Exists(Server.MapPath(strCurrPath)))
+            if (!strCurrPath.StartsWith("http") && File.Exists(Server.MapPath(strCurrPath)))
             {
                 var fileInfo = new FileInfo(Server.MapPath(strCurrPath));
                 var dir = fileInfo.Directory.FullName;

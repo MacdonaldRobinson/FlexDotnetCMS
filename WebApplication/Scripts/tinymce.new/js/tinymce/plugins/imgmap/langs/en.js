9aab263ebe7ecda633be3591ebb21dd006a9b331
mac <macdonald.robinson@zgm.ca>
2018-03-13 11:50:46 -0600

    Removed tinymce backup

--- WebApplication/Scripts/tinymce.new/js/tinymce/plugins/imgmap/langs/en.js
+++
@@ -1,7 +0,0 @@
-// UK lang variables
-
-tinyMCE.addI18n('en.imgmap', {
-	title : 'Image Map Editor',
-	desc : 'Image Map Editor',
-	remove : 'Remove map'
-});
--- WebApplication/Scripts/tinymce.new/js/tinymce/plugins/imgmap/langs/fr.js
+++
@@ -1,7 +0,0 @@
-// UK lang variables
-
-tinyMCE.addI18n('fr.imgmap', {
-	title : 'Image Map Editor',
-	desc : 'Image Map Editor',
-	remove : 'Supprimer la map'
-});
